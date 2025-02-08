ae500ddd2814f097fae3d4cc812822de94f65721
unknown <omp13821380@gmail.com>
2025-02-08 11:37:28 +0330

    '

--- src/app/components/SinglePost.tsx
+++ src/app/components/SinglePost.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { IconComment, IconDirect, IconEmoji, IconHeart, IconMore, IconSave } from "./Icons";
 import { useTranslation } from "next-i18next";
 import test from "node:test";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { list } from "postcss";
 import { Interface } from "readline";
 
@@ -75,9 +75,26 @@ export function PostAction(){
     )
 }
 export function CommentInput(){
-    const [value, setValue] = useState("");
+    const [value, setValue] = useState<string>("");
+    const [emojiBottom,setEmojiBottom] = useState<boolean>(false)
+    const [emojiBoxToggle,setEmojiBoxToggle] = useState<boolean>(false)
     const textareaRef = useRef<HTMLTextAreaElement>(null);
-
+    const emojiBoxRef = useRef<HTMLDivElement>(null);
+    const handleClickOutside = (e: MouseEvent) => {
+        if (emojiBoxRef.current && !emojiBoxRef.current.contains(e.target as Node)) {
+            console.log('clicked outside')
+            setEmojiBoxToggle(false);
+        }
+      };
+    useEffect(() => {
+        console.log('test')
+        if (emojiBoxToggle) {
+          document.addEventListener("click", handleClickOutside);
+        } else {
+          document.removeEventListener("click", handleClickOutside);
+        }
+        return () => document.removeEventListener("click", handleClickOutside);
+      }, [emojiBoxToggle]);
     const handleInput = (e : React.ChangeEvent<HTMLTextAreaElement>) => {
         setValue(e.target.value);
         adjustTextareaHeight();
@@ -90,25 +107,52 @@ export function CommentInput(){
         }
     };
     const test = (e : React.MouseEvent<HTMLDivElement>) => {
-        console.log(e.target)
+        const element = e.target as HTMLElement
+        const rect = element.getBoundingClientRect();
+        const distanceFromTop = rect.top;
+        const distanceFromBottom = window.innerHeight - rect.bottom;
+        
+        if(distanceFromTop > distanceFromBottom) {
+            setEmojiBottom(true)
+        }
+        else{
+            setEmojiBottom(false)
+        }
+        setEmojiBoxToggle(!emojiBoxToggle)
     }
+    const insertEmoji = (emoji: string) => {
+        if (textareaRef.current) {
+          const textarea = textareaRef.current;
+          const start = textarea.selectionStart;
+          const end = textarea.selectionEnd;
+          const newValue =
+            value.substring(0, start) + emoji + value.substring(end);
+          setValue(newValue);
+
+          setTimeout(() => {
+            textarea.setSelectionRange(start + emoji.length, start + emoji.length);
+            textarea.focus();
+          }, 0);
+        }
+      };
     const { t } = useTranslation()
     return(
         <div className="border-t-[1px] border-ss mx-2 py-[6px] items-center flex">
             <div className="relative cursor-pointer">
                 <div onClick={test} className="py-2 px-4 inline-block"><IconEmoji/></div>
-                <EmojiBox/>
+                {emojiBoxToggle && <EmojiBox insertEmoji={insertEmoji} emojiBoxRef={emojiBoxRef} isBottom0={emojiBottom}/>}
             </div>
             <textarea ref={textareaRef} className={`resize-none outline-none max-h-20 flex-1 text-sm items-center flex pr-1`} rows={1} onInput={handleInput} placeholder={t('add-comment')} value={value} name="" id=""></textarea>
             <span>Post</span>
-            
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
         </div>
     )
 }
 
 
 
-export function EmojiBox(){
+export function EmojiBox({isBottom0,emojiBoxRef,insertEmoji} : {isBottom0? : boolean,emojiBoxRef : React.Ref<HTMLDivElement>,insertEmoji:(emoji: string) => void}) {
+    
     const emojiList : {'name':string,'emoji':string[]}[] = [
         {
             "name":"Most popular",
@@ -145,17 +189,17 @@ export function EmojiBox(){
     ]
     
     return(
-        <div className="absolute w-[342] h-[325px] bg-white shadow-[0_4px_12px_rgba(0,0,0,.15)] p-2 overflow-y-scroll rounded-lg z-50 bottom-10">
+        <div ref={emojiBoxRef} className={`absolute w-[342] h-[325px] bg-white shadow-[0_4px_12px_rgba(0,0,0,.15)] p-2 overflow-y-scroll rounded-lg z-50 ${isBottom0 && 'bottom-10'}`}>
             {emojiList.map((category,index)=>{
                 return(
                     <div key={index} className="mb-4">
                         <h2 className="text-sm text-gray font-semibold mb-2">{category.name}</h2>
                         <div className="flex flex-wrap">
                             {category.emoji.map((emoji, i) => (
-                            <span key={i} className="size-[44px] items-center justify-center flex text-[32px] hover:bg-zinc-200 cursor-pointer">{emoji}</span>
+                            <span onClick={()=>insertEmoji(emoji)} key={i} className="size-[44px] items-center justify-center flex text-[32px] hover:bg-zinc-200 cursor-pointer">{emoji}</span>
                             ))}
                         </div>
-                        </div>
+                    </div>
                 )
             })}
         </div>        
