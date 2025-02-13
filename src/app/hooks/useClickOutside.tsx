8f96d51665ecf80313580ed5c207fe23268dbe2a
unknown <omp13821380@gmail.com>
2025-02-13 18:32:02 +0330

    its a simple hook to handle outside clicks like clicking outside of a popup to close it, the hook recive ref and action

---
+++ src/app/hooks/useClickOutside.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+
+export function useClickOutside(ref: React.RefObject<HTMLElement | null>, callback: () => void) {
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        callback();
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [ref, callback]);
+}
