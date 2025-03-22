5fc3827b91fe9072840cd8fe217aaa0e6daf8532
unknown <omp13821380@gmail.com>
2025-03-22 17:36:14 +0330

    '

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,17 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
-    reactStrictMode: true
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+          {
+            protocol: "http",
+            hostname: "localhost",
+            port: "8000",
+          },
+        ],
+        domains: ["localhost"],
+      },
+
 };
 
 export default nextConfig;
