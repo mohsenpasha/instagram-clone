4252fd163f244815e77d14599dab1884ba918a66
unknown <omp13821380@gmail.com>
2025-03-22 17:40:01 +0330

    type added

--- src/app/components/PostList.tsx
+++ src/app/components/PostList.tsx
@@ -1,21 +1,19 @@
 'use client'
 import { List } from "postcss/lib/list";
 import PostPreview from "./PostPreview";
+import { PostPopupSlider } from "./PostPopupSlider";
+import { useRef } from "react";
 type ListType = {
     isReel: boolean,
-    noIcon?:boolean
+    noIcon?:boolean,
+    postList:[]
 }
-export default function PostList({isReel,noIcon}:ListType){
-    console.log(isReel)
+export default function PostList({isReel,noIcon,postList}:ListType){
     return(
-        <div className="flex w-full gap-[4px] flex-wrap">
-            <PostPreview noIcon={noIcon} isReel={isReel}/>
-            <PostPreview noIcon={noIcon} isReel={isReel}/>
-            <PostPreview noIcon={noIcon} isReel={isReel}/>
-            {/* <PostPreview noIcon={noIcon} isReel={isReel}/> 
-            <PostPreview noIcon={noIcon} isReel={isReel}/> 
-            <PostPreview noIcon={noIcon} isReel={isReel}/> 
-            <PostPreview noIcon={noIcon} isReel={isReel}/>  */}
+        <div className="flex pb-12 md:pb-0 w-full gap-[4px] flex-wrap">
+            {postList?.map((item,index)=>{
+                return <PostPreview key={index} postDetail={item} noIcon={noIcon} isReel={isReel}/>
+            })}
         </div>
     )
 }
