63009947d7435644f05bfe1da37a0fa74899f69a
unknown <omp13821380@gmail.com>
2025-03-29 03:15:14 +0330

    adding privatePage and nopost page template and also adding follower and following list view for pages with greater than 0 follower or following which are not private

--- src/app/[id]/layout.tsx
+++ src/app/[id]/layout.tsx
@@ -8,40 +8,56 @@ import { useTranslation } from "next-i18next";
 import i18n  from '@/../../i18n'
 import { IconPosts, IconReels, IconTagged } from "@/components/Icons";
 import StoryHolder, { StoryList } from "@/components/Story";
+import { changeCurrentVisitingUser } from "@/store/slices/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchGetUserInfo } from "@/api/userInfo";
+import { RootState } from "@/store/store";
 
 
 export default function ProfileLayout({children} : {children : React.ReactNode}){
     const params = useParams()
     const { t } = useTranslation();
-
+    const dispatch = useDispatch()
+    const userInfo = useSelector((state: RootState) => state.currentUser.currentVisitingUser);
+    async function getUserData(){
+        const response = await fetchGetUserInfo(params.id)
+        const jsonRes = await response.json()
+        dispatch(changeCurrentVisitingUser(jsonRes))
+    }
     useEffect(()=>{
-        // i18n.changeLanguage("fa")
+        getUserData()
     },[])
     return(
             <div className={`flex justify-between`}>
                 <SideBar/>
-                <div className="flex justify-center w-full md:w-[100vw-72px] xl:w-10/12">
-                    <div className="w-full md:px-4 xl:px-0 lg:w-[930px]">
-                        <ProfileHeader/>
-                        <div className="flex w-full border-t-[1px] border-d justify-center">
-                            <div className="w-full md:w-6/12 justify-around flex">
-                                <Link className="py-4 border-black mt-[-1px] text-gray border-t-[1px] w-4/12 md:w-fit flex justify-center gap-[6px] items-center" href={`/${params.id}/#`}>
-                                    <IconPosts className="size-[12px]" />
-                                    {t('post-t')}
-                                </Link>
-                                <Link className="py-4 border-black mt-[-1px] text-gray border-t-[1px] w-4/12 md:w-fit flex justify-center gap-[6px] items-center" href={`/${params.id}/reels`}>
-                                    <IconReels className="size-[12px]" />
-                                    REELS
-                                </Link>
-                                <Link className="py-4 border-black mt-[-1px] text-gray border-t-[1px] w-4/12 md:w-fit flex justify-center gap-[6px] items-center" href={`/${params.id}/tagged`}>
-                                    <IconTagged className="size-[12px]" />
-                                    {t('tagged')}
-                                </Link>
-                            </div>
+                {userInfo ? 
+                    <div className="flex justify-center w-full md:w-[100vw-72px] xl:w-10/12">
+                        <div className="w-full md:px-4 xl:px-0 lg:w-[930px]">
+                            <ProfileHeader/>
+                            {!userInfo.is_private &&
+                                <div className="flex w-full justify-center">
+                                    <div className="w-full md:w-6/12 justify-around flex">
+                                        <Link className="py-4 border-black mt-[-1px] text-gray border-t-[1px] w-4/12 md:w-fit flex justify-center gap-[6px] items-center" href={`/${params.id}/#`}>
+                                            <IconPosts className="size-[12px]" />
+                                            {t('post-t')}
+                                        </Link>
+                                        <Link className="py-4 border-black mt-[-1px] text-gray border-t-[1px] w-4/12 md:w-fit flex justify-center gap-[6px] items-center" href={`/${params.id}/reels`}>
+                                            <IconReels className="size-[12px]" />
+                                            REELS
+                                        </Link>
+                                        <Link className="py-4 border-black mt-[-1px] text-gray border-t-[1px] w-4/12 md:w-fit flex justify-center gap-[6px] items-center" href={`/${params.id}/tagged`}>
+                                            <IconTagged className="size-[12px]" />
+                                            {t('tagged')}
+                                        </Link>
+                                    </div>
+                                </div>
+                            }
+                            {children}
                         </div>
-                        {children}
                     </div>
-                </div>
+                    :
+                    <div>loading</div>
+                }
                 {/* <StoryList /> */}
             </div>
     )
--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import PostList from "@/components/PostList"
-import SinglePost from "@/components/SinglePost"
+import SinglePost, { UnfollowPopup, UserList } from "@/components/SinglePost"
 import { useEffect, useRef, useState } from "react"
 import { useParams } from 'next/navigation'
 import { PostPopupSlider } from "@/components/PostPopupSlider"
@@ -9,16 +9,42 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/store/store";
 import { useRouter } from "next/navigation";
 import { disableScroll, enableScroll } from "@/utils/scroll"
+import { IconCamera, IconPrivatePage } from "@/components/Icons"
+import { FollowBtn } from "@/components/FollowBtn"
+import { fetchGetUserInfo } from "@/api/userInfo"
+import { useClickOutside } from "@/hooks/useClickOutside"
+import { changeListTitle, changeListUrl, clearUserList } from "@/store/slices/postSlice"
+import { changeUnfollow } from "@/store/slices/userSlice"
 
 export default function Profile(){
     const router = useRouter();
     const popupPost = useSelector((state: RootState) => state.popupPost.value);
+    const userInfo = useSelector((state: RootState) => state.currentUser.currentVisitingUser);
+    const unfollowDetail = useSelector((state: RootState) => state.currentUser.unfollowDetail);
+    const listTitle = useSelector((state: RootState) => state.popupPost.listTitle);
+    const userListRef = useRef(null)
+    const unfollowPopupRef = useRef<HTMLElement | null>(null)
+    useClickOutside(userListRef, () => !unfollowDetail ? dispatch(changeListTitle(null)) : {});
+    useClickOutside(unfollowPopupRef, () => dispatch(changeUnfollow(null)));
+    const [userListToggle,setUserListToggle] = useState(false)
     const dispatch = useDispatch();
     const params = useParams()
     const postRef = useRef(`http://localhost:8000/${params.id}/posts`)
     const [postList,setPostList] = useState([])
     const [isLoading, setIsLoading] = useState(false);
-    const [hasMore, setHasMore] = useState(true);
+    const [isUnfollowInList,setIsUnfollowInList] = useState(false)
+    useEffect(()=>{
+        if(listTitle){
+            setUserListToggle(true)
+            setIsUnfollowInList(true)
+        }
+        else{
+            setIsUnfollowInList(false)
+            dispatch(clearUserList())
+            dispatch(changeListUrl(null))
+            setUserListToggle(false)
+        }
+    },[listTitle])
     async function fetchPosts(){
         const response = await fetch(postRef.current, {
             method: "GET",
@@ -32,14 +58,16 @@ export default function Profile(){
         setPostList((prevPosts) => [...prevPosts, ...jsonRes.results]);
         setIsLoading(false);
     }
+    
     useEffect(()=>{
-        console.log('startup shit')
-        fetchPosts()
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
+        console.log(userInfo)
+        if(!userInfo.is_private){
+            fetchPosts()
+            window.addEventListener("scroll", handleScroll);
+            return () => window.removeEventListener("scroll", handleScroll);
+        }
     },[])
     useEffect(()=>{
-        console.log(popupPost)
         if(popupPost){
             disableScroll()
         }
@@ -62,14 +90,63 @@ export default function Profile(){
             fetchPosts()
         }
       }, [isLoading]);
-
+      if(!userInfo) return
     return(
         <>
-            <PostList postList={postList} isReel={false} />
-            {popupPost && 
-                <PostPopupSlider/>
+        {userInfo.is_private ?
+        <div className="flex flex-col justify-center items-center gap-4">
+            <PrivatePage/>
+            <FollowBtn className="w-11/12 md:w-auto text-sm md:text-base" haveIcon={false} inList={false} userData={userInfo}/>
+        </div>
+        :
+        <>
+            {!userInfo.post_count ? 
+                <NoPost/>
+                :
+                <>
+                <PostList postList={postList} isReel={false} />
+                {popupPost && 
+                    <PostPopupSlider/>
+                }
+                </>
             }
-
         </>
+        }
+        {userListToggle && 
+            <UserList listType={listTitle == 'Followers' ? 'followerList' : 'followingList'} targetId={userInfo.username} ref={userListRef} closePopup={()=>dispatch(changeListTitle(null))} />
+        }
+        {unfollowDetail &&
+            <UnfollowPopup inList={isUnfollowInList} ref={unfollowPopupRef}/>
+        }
+        </>
+    )
+}
+
+export function PrivatePage(){
+    return(
+        <div className="w-full flex justify-center items-center pt-6">
+            <div className="flex w-fit gap-2 items-center">
+                <IconPrivatePage/>
+                <div className="flex flex-col gap-[2px] text-sm">
+                    <span className="font-semibold">This account is private</span>
+                    <span className="text-gray">Follow to see their photos and videos.</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export function NoPost(){
+    const userInfo = useSelector((state: RootState) => state.currentUser.currentVisitingUser);
+    return(
+        <div className="w-full flex justify-center items-center md:my-14 md:p-0 p-2 border-t-[1px] border-b-[1px] border-ss">
+            <div className="flex md:flex-col w-full md:w-fit gap-2 items-center md:justify-center">
+                <IconCamera className="size-16 flex-shrink-0"/>
+                <div className="flex flex-col text-sm md:mt-8 md:mb-12">
+                    <span className="md:font-black font-medium md:text-3xl">No Posts Yet</span>
+                    <span className="text-gray text-sm md:hidden block">When {userInfo.username} posts, you'll see their photos and videos here</span>
+                </div>
+            </div>
+        </div>
     )
 }
