78df1475b655a2eb0369094223e1a789063c4661
unknown <omp13821380@gmail.com>
2025-03-29 03:08:57 +0330

    now we can handle both inside list(likelist popup for instance) follow action and a single user follow from user page

--- src/app/api/followApi.tsx
+++ src/app/api/followApi.tsx
@@ -1,10 +1,12 @@
-import { toggleIsLoading } from "@/store/slices/postSlice";
-
-export async function fetchFollowUser(username:string,dispatch:any){
-    console.log('test start point')
-    console.log('test before dispatch')
-    dispatch(toggleIsLoading({username:username,result:true}))
-    console.log('test after dispatch')
+import { listToggleIsLoading } from "@/store/slices/postSlice";
+import { toggleIsLoading } from "@/store/slices/userSlice";
+export async function fetchFollowUser(username:string,dispatch:any,inList=true){
+    if(inList){
+        dispatch(listToggleIsLoading({username:username,result:true}))
+    }
+    else{
+        dispatch(toggleIsLoading(true))
+    }
     const response = await fetch(`http://localhost:8000/follow/${username}`, {
         method: "post",
         credentials: "include",
@@ -12,12 +14,22 @@ export async function fetchFollowUser(username:string,dispatch:any){
             "Content-Type": "application/json",
         },
     });
-    dispatch(toggleIsLoading({username:username,result:false}))
+    if(inList){
+        dispatch(listToggleIsLoading({username:username,result:false}))
+    }
+    else{
+        dispatch(toggleIsLoading(false))
+    }
     return(response)
 }
 
-export async function fetchUnfollowUser(username:string,dispatch:any){
-    dispatch(toggleIsLoading({username:username,result:true}))
+export async function fetchUnfollowUser(username:string,dispatch:any,inList=true){
+    if(inList){
+        dispatch(listToggleIsLoading({username:username,result:true}))
+    }
+    else{
+        dispatch(toggleIsLoading(true))
+    }
     const response = await fetch(`http://localhost:8000/unfollow/${username}`, {
         method: "post",
         credentials: "include",
@@ -25,7 +37,12 @@ export async function fetchUnfollowUser(username:string,dispatch:any){
             "Content-Type": "application/json",
         },
     });
-    dispatch(toggleIsLoading({username:username,result:false}))
+    if(inList){
+        dispatch(listToggleIsLoading({username:username,result:false}))
+    }
+    else{
+        dispatch(toggleIsLoading(false))
+    }
     return(response)
 }
 
