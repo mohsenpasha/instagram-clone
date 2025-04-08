e1b89f884413e83a854dccd7762d9ec321acd856
unknown <omp13821380@gmail.com>
2025-04-08 22:19:40 +0330

    tagged icon has been added

--- src/app/components/Icons.tsx
+++ src/app/components/Icons.tsx
@@ -182,3 +182,10 @@ export function IconPlusCircle({className}:IconType){
     )
 }
 
+export function IconUser({className}:IconType){
+    return(
+        <svg className={className} fill="currentColor" height="12" role="img" viewBox="0 0 24 24" width="12"><path d="M21.334 23H2.666a1 1 0 0 1-1-1v-1.354a6.279 6.279 0 0 1 6.272-6.272h8.124a6.279 6.279 0 0 1 6.271 6.271V22a1 1 0 0 1-1 1ZM12 13.269a6 6 0 1 1 6-6 6.007 6.007 0 0 1-6 6Z"></path></svg>
+    )
+}
+
+
