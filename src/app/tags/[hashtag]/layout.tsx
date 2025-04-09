d75141196b43cdfee9dee32951d2be111df50819
unknown <omp13821380@gmail.com>
2025-04-09 17:16:52 +0330

    nothing much

--- src/app/tags/[hashtag]/layout.tsx
+++ src/app/tags/[hashtag]/layout.tsx
@@ -12,7 +12,6 @@ export default function PostLayout({children}:{children:React.ReactNode}){
                         {children}
                     </div>
                 </div>
-
             </div>
     )
 }
