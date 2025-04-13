50b21fa784ec973178326fe6083d1fc0b6f76929
unknown <omp13821380@gmail.com>
2025-04-13 14:50:45 +0330

    adding post create to sidebar uncomplete

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -13,6 +13,7 @@ import { RootState } from "@/store/store";
 import { toggleSearch } from "@/store/slices/searchSlice";
 import { addNotification, toggleNotification } from "@/store/slices/notificationSlice";
 import NotificationBar from "./NotificationBar";
+import CreatePostPopup from "./PostCreate";
 export default function SideBar({isAlwaysMinimal}:{isAlwaysMinimal?:boolean}){
     const isSearchCloseAnimationStarted = useSelector((state: RootState) => state.searchInfo.isSearchCloseAnimationStarted)
     const isNotificationCloseAnimationStarted = useSelector((state: RootState) => state.notificationInfo.isNotificationCloseAnimationStarted)
@@ -118,120 +119,122 @@ export default function SideBar({isAlwaysMinimal}:{isAlwaysMinimal?:boolean}){
     useClickOutside(searchRef, () => SearchBarToggle());
     useClickOutside(NotificationRef, () => notificationBarToggle());
     return(
-        <div className={`${isMinimal ? 'md:w-[74px] w-full' : 'w-full md:w-fit xl:w-2/12'} transition-all duration-300 fixed z-50 h-fit bottom-0 md:sticky md:top-0 bg-white px-[12px] py-[2px] md:py-[8px] md:pb-[20px] border-t-[1px] md:border-t-0 md:border-x-[1px] border-ss md:h-screen`}>
-            <div className="flex flex-col justify-between h-full">
-            <div className="hidden md:block p-[12px] pt-[25px] pb-[16px]">
+        <>
+            <div className={`${isMinimal ? 'md:w-[74px] w-full' : 'w-full md:w-fit xl:w-2/12'} transition-all duration-300 fixed z-50 h-fit bottom-0 md:sticky md:top-0 bg-white px-[12px] py-[2px] md:py-[8px] md:pb-[20px] border-t-[1px] md:border-t-0 md:border-x-[1px] border-ss md:h-screen`}>
+                <div className="flex flex-col justify-between h-full">
+                <div className="hidden md:block p-[12px] pt-[25px] pb-[16px]">
 
-                {!isMinimal &&
-                    <Image className="hidden xl:block w-28" src='/images/instagram.svg' alt="" width={500} height={300} />
-                }
-                <IconInstagram className={`${isMinimal ? 'block' : 'block xl:hidden'}`}/>
-            </div>
-                {isSearchActive &&
-                    <SearchBar ref={searchRef} closeAnimationStat={isSearchCloseAnimationStarted}/>
-                }
-                {isNotificationActive && 
-                    <NotificationBar ref={NotificationRef} closeAnimationStat={isNotificationCloseAnimationStarted}/>
-                }
-                <ul className="flex w-full justify-around md:block">
-                    {/* <button onClick={() => i18n.changeLanguage("fa")}>فارسی</button>
-                    <button onClick={() => i18n.changeLanguage("en")}>English</button> */}
-
-                    <li>
-                        <Link title={t('home')} href="#" className="flex gap-4 p-0 md:p-[12px] my-[10px] rounded-lg hover:bg-zinc-100 transition-all
-">
-                            <IconHome className={'shrink-0'}/>
-                            {!isMinimal &&
-                            <span className="hidden xl:inline-block">{t('home')}</span>
-                            }
-                        </Link>
-                    </li>
-                    <li className="hidden md:block">
-                        <div onClick={()=>SearchBarToggle(!isNotificationActive)} title={t('search')} className="flex gap-4 p-0 md:p-[12px] my-[10px] rounded-lg hover:bg-zinc-100 transition-all
-">
-                            <IconSearch className={'shrink-0'}/>
-                            {!isMinimal &&
-                                <span className="hidden xl:inline-block">{t('search')}</span>
-                            }
-                        </div>
-                    </li>
-                    <li>
-                        <Link title={t('explore')} href="#" className="flex gap-4 p-0 md:p-[12px] my-[10px] rounded-lg hover:bg-zinc-100 transition-all
-">
-                            <IconExplore className={'shrink-0'} />
-                            {!isMinimal &&
-                                <span className="hidden xl:inline-block">{t('explore')}</span>
-                            }
-                        </Link>
-                    </li>
-                    <li>
-                        <Link title={t('reels')} href="#" className="flex gap-4 p-0 md:p-[12px] my-[10px] rounded-lg hover:bg-zinc-100 transition-all
-">
-                            <IconReels className={'shrink-0'} />
-                            {!isMinimal &&
-                                <span className="hidden xl:inline-block">{t('reels')}</span>
-                            }
-                        </Link>
-                    </li>
-                    <li className="block md:hidden">
-                        <Link title={t('create')} href="#" className="flex gap-4 p-0 md:p-[12px] my-[10px] rounded-lg hover:bg-zinc-100 transition-all
-">
-                            <IconAdd className={'shrink-0'}/>
-                            {!isMinimal &&
-                                <span className="hidden xl:inline-block">{t('create')}</span>
-                            }
-                        </Link>
-                    </li>
-                    <li>
-                        <Link title={t('direct')} href="#" className="flex gap-4 p-0 md:p-[12px] my-[10px] rounded-lg hover:bg-zinc-100 transition-all
-">
-                            <IconDirect className={'shrink-0'} />
-                            {!isMinimal &&
-                                <span className="hidden xl:inline-block">{t('direct')}</span>
-                            }
-                        </Link>
-                    </li>
-                    <li className="hidden md:block">
-                        <div onClick={()=>notificationBarToggle(!isSearchActive)} title={t('notification')} className="relative flex gap-4 p-0 md:p-[12px] my-[10px] rounded-lg hover:bg-zinc-100 transition-all
-">
-                            {newNotificationCount != 0 && 
-                                <UnSeenCounter counter={newNotificationCount}/>
-                            }
-                            <IconHeart className={'shrink-0'}/>
-                            {!isMinimal &&
-                                <span className="hidden xl:inline-block">{t('notification')}</span>
-                            }
-                        </div>
-                    </li>
-                    <li className="hidden md:block">
-                        <Link title={t('create')} href="#" className="flex gap-4 p-0 md:p-[12px] my-[10px] rounded-lg hover:bg-zinc-100 transition-all
-">
-                            <IconAdd className={'shrink-0'} />
-                            {!isMinimal &&
-                                <span className="hidden xl:inline-block">{t('create')}</span>
-                            }
-                        </Link>
-                    </li>
-                    <li>
-                        <Link title={t('profile')} href="#" className="flex gap-4 p-0 md:p-[12px] my-[10px] rounded-lg hover:bg-zinc-100 transition-all
-">
-                            <IconAdd className={'shrink-0'} />
-                            {!isMinimal &&
-                                <span className="hidden xl:inline-block">{t('profile')}</span>
-                            }
-                        </Link>
-                    </li>
-                </ul>
-                <div title={t('menu')} className="hidden md:flex gap-4 p-0 md:p-[12px] my-[10px] mt-16 rounded-lg hover:bg-zinc-100 transition-all
- cursor-pointer">
-                    <IconMenu className={'shrink-0'} />
                     {!isMinimal &&
-                        <span className="hidden xl:inline-block">{t('menu')}</span>
+                        <Image className="hidden xl:block w-28" src='/images/instagram.svg' alt="" width={500} height={300} />
                     }
+                    <IconInstagram className={`${isMinimal ? 'block' : 'block xl:hidden'}`}/>
+                </div>
+                    {isSearchActive &&
+                        <SearchBar ref={searchRef} closeAnimationStat={isSearchCloseAnimationStarted}/>
+                    }
+                    {isNotificationActive && 
+                        <NotificationBar ref={NotificationRef} closeAnimationStat={isNotificationCloseAnimationStarted}/>
+                    }
+                    <ul className="flex w-full justify-around md:block">
+                        {/* <button onClick={() => i18n.changeLanguage("fa")}>فارسی</button>
+                        <button onClick={() => i18n.changeLanguage("en")}>English</button> */}
+
+                        <li>
+                            <Link title={t('home')} href="#" className="flex gap-4 p-0 md:p-[12px] my-[10px] rounded-lg hover:bg-zinc-100 transition-all
+    ">
+                                <IconHome className={'shrink-0'}/>
+                                {!isMinimal &&
+                                <span className="hidden xl:inline-block">{t('home')}</span>
+                            }
+                            </Link>
+                        </li>
+                        <li className="hidden md:block">
+                            <div onClick={()=>SearchBarToggle(!isNotificationActive)} title={t('search')} className="flex gap-4 p-0 md:p-[12px] my-[10px] rounded-lg hover:bg-zinc-100 transition-all
+    ">
+                                <IconSearch className={'shrink-0'}/>
+                                {!isMinimal &&
+                                    <span className="hidden xl:inline-block">{t('search')}</span>
+                                }
+                            </div>
+                        </li>
+                        <li>
+                            <Link title={t('explore')} href="#" className="flex gap-4 p-0 md:p-[12px] my-[10px] rounded-lg hover:bg-zinc-100 transition-all
+    ">
+                                <IconExplore className={'shrink-0'} />
+                                {!isMinimal &&
+                                    <span className="hidden xl:inline-block">{t('explore')}</span>
+                                }
+                            </Link>
+                        </li>
+                        <li>
+                            <Link title={t('reels')} href="#" className="flex gap-4 p-0 md:p-[12px] my-[10px] rounded-lg hover:bg-zinc-100 transition-all
+    ">
+                                <IconReels className={'shrink-0'} />
+                                {!isMinimal &&
+                                    <span className="hidden xl:inline-block">{t('reels')}</span>
+                                }
+                            </Link>
+                        </li>
+                        <li className="block md:hidden">
+                            <Link title={t('create')} href="#" className="flex gap-4 p-0 md:p-[12px] my-[10px] rounded-lg hover:bg-zinc-100 transition-all
+    ">
+                                <IconAdd className={'shrink-0'}/>
+                                {!isMinimal &&
+                                    <span className="hidden xl:inline-block">{t('create')}</span>
+                                }
+                            </Link>
+                        </li>
+                        <li>
+                            <Link title={t('direct')} href="#" className="flex gap-4 p-0 md:p-[12px] my-[10px] rounded-lg hover:bg-zinc-100 transition-all
+    ">
+                                <IconDirect className={'shrink-0'} />
+                                {!isMinimal &&
+                                    <span className="hidden xl:inline-block">{t('direct')}</span>
+                                }
+                            </Link>
+                        </li>
+                        <li className="hidden md:block">
+                            <div onClick={()=>notificationBarToggle(!isSearchActive)} title={t('notification')} className="relative flex gap-4 p-0 md:p-[12px] my-[10px] rounded-lg hover:bg-zinc-100 transition-all
+    ">
+                                {newNotificationCount != 0 && 
+                                    <UnSeenCounter counter={newNotificationCount}/>
+                                }
+                                <IconHeart className={'shrink-0'}/>
+                                {!isMinimal &&
+                                    <span className="hidden xl:inline-block">{t('notification')}</span>
+                                }
+                            </div>
+                        </li>
+                        <li className="hidden md:block">
+                            <Link title={t('create')} href="#" className="flex gap-4 p-0 md:p-[12px] my-[10px] rounded-lg hover:bg-zinc-100 transition-all
+    ">
+                                <IconAdd className={'shrink-0'} />
+                                {!isMinimal &&
+                                    <span className="hidden xl:inline-block">{t('create')}</span>
+                                }
+                            </Link>
+                        </li>
+                        <li>
+                            <Link title={t('profile')} href="#" className="flex gap-4 p-0 md:p-[12px] my-[10px] rounded-lg hover:bg-zinc-100 transition-all
+    ">
+                                <IconAdd className={'shrink-0'} />
+                                {!isMinimal &&
+                                    <span className="hidden xl:inline-block">{t('profile')}</span>
+                                }
+                            </Link>
+                        </li>
+                    </ul>
+                    <div title={t('menu')} className="hidden md:flex gap-4 p-0 md:p-[12px] my-[10px] mt-16 rounded-lg hover:bg-zinc-100 transition-all
+    cursor-pointer">
+                        <IconMenu className={'shrink-0'} />
+                        {!isMinimal &&
+                            <span className="hidden xl:inline-block">{t('menu')}</span>
+                        }
+                    </div>
                 </div>
             </div>
-
-        </div>
+            <CreatePostPopup />
+        </>
     )
 }
 
