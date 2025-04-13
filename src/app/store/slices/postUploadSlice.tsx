625bcabe1e9853cee7cc20f8f717dea1e213502b
unknown <omp13821380@gmail.com>
2025-04-13 23:08:22 +0330

    now you can swap media with each other by draging them

--- src/app/store/slices/postUploadSlice.tsx
+++ src/app/store/slices/postUploadSlice.tsx
@@ -1,8 +1,16 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { stat } from "fs";
 
 type PostUpload = {
-  postMedia:{} | null,
+  postMedia:[
+    {
+      name: string,
+      size: string,
+      type: string,
+      previewUrl: string,
+      order:number
+    }
+  ] | null,
   activeIndex:number | null
 }
 const initialState : PostUpload = {
@@ -26,10 +34,25 @@ const createPostSlice = createSlice({
       state.activeIndex = action.payload
     },
     deleteActiveIndex: (state)=>{
-      state.postMedia = state.postMedia.filter((_, index) => index !== state.activeIndex);     
-    }
+      state.postMedia = state.postMedia.filter((_, index) => index !== state.activeIndex);
+    },
+    swapPostMedia: (state, action: PayloadAction<{ from: number; to: number }>) => {
+      if (!state.postMedia) return;
+
+      const { from, to } = action.payload;
+
+      if (
+        from < 0 || to < 0 ||
+        from >= state.postMedia.length ||
+        to >= state.postMedia.length
+      ) return;
+
+      const temp = state.postMedia[from];
+      state.postMedia[from] = state.postMedia[to];
+      state.postMedia[to] = temp;
+    },
   },
 });
 
-export const { addPostMedia, changeActiveIndex, deleteActiveIndex} = createPostSlice.actions;
+export const { addPostMedia, changeActiveIndex, deleteActiveIndex, swapPostMedia} = createPostSlice.actions;
 export default createPostSlice.reducer;
