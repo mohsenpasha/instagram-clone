ecc9c794e6448ede8a78d2847d7c21ad79d15a24
unknown <omp13821380@gmail.com>
2025-04-15 15:40:13 +0330

    now hook returns event

--- src/app/hooks/useClickOutside.tsx
+++ src/app/hooks/useClickOutside.tsx
@@ -2,14 +2,14 @@ import { useEffect } from "react";
 
 export function useClickOutside(
   refs: React.RefObject<HTMLElement | null> | React.RefObject<HTMLElement | null>[],
-  callback: () => void
+  callback: (event: MouseEvent) => void
 ) {
   useEffect(() => {
     const handleClickOutside = (e: MouseEvent) => {
       const refArray = Array.isArray(refs) ? refs : [refs];
 
       if (refArray.every(ref => ref.current && !ref.current.contains(e.target as Node))) {
-        callback();
+        callback(e);
       }
     };
 
