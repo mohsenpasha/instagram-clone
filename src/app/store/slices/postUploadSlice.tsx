dfeda4575e6f7941a5cf7c8155ffbfc943493837
unknown <omp13821380@gmail.com>
2025-04-15 15:41:05 +0330

    adding a few neccessery variables to postcreate slice

--- src/app/store/slices/postUploadSlice.tsx
+++ src/app/store/slices/postUploadSlice.tsx
@@ -8,6 +8,16 @@ type PostUpload = {
     type: string;
     previewUrl: string;
     order: number;
+    alt?:string,
+    tagged_user?:[
+      {
+        username:string,coordinates:
+        {
+          x:number,
+          y:number
+        }
+      }
+  ],
     transform?: {
       position: { x: number, y: number };
       scale: number;
@@ -15,10 +25,17 @@ type PostUpload = {
     croppedDataURL?: string;
   }[] | null;
   activeIndex: number | null;
+  addingTaggedUser?:false,
+  addingTagCoordinates :{
+    x:number,
+    y:number
+  }
 };
 const initialState : PostUpload = {
   postMedia:null,
-  activeIndex:null
+  activeIndex:null,
+  addingTaggedUser:false,
+  addingTagCoordinates:{x:0,y:0},
 };
 
 const createPostSlice = createSlice({
@@ -66,8 +83,63 @@ const createPostSlice = createSlice({
         state.postMedia[index].croppedDataURL = croppedDataURL;
       }
     },
+    changeMediaAlt: (state, action) => {
+      const { index, alt } = action.payload;
+      if (state.postMedia && state.postMedia[index]) {
+        state.postMedia[index].alt = alt;
+      }
+    },
+    addMediaUserTag: (state, action) => {
+      const { username, coordinates } = action.payload;
+      const index = state.activeIndex;
+    
+      if (state.postMedia && index !== null && state.postMedia[index]) {
+        const currentMedia = state.postMedia[index];
+        currentMedia.tagged_user = [
+          ...(currentMedia.tagged_user || []),
+          { username, coordinates }
+        ];
+      }
+    },
+    removeMediaUserTag: (state, action) => {
+      const index = state.activeIndex;
+    
+      if (state.postMedia && index !== null && state.postMedia[index]) {
+        const currentMedia = state.postMedia[index];
+        if(!currentMedia.tagged_user) return
+        currentMedia.tagged_user = currentMedia.tagged_user.filter((item)=> item.username != action.payload ? true : false)
+      }
+    },
+    moveMediaUserTagCoordinates: (state, action) => {
+      const index = state.activeIndex;
+      if (state.postMedia && index !== null && state.postMedia[index]) {
+        const currentMedia = state.postMedia[index];
+        if(!currentMedia.tagged_user) return
+        currentMedia.tagged_user = currentMedia.tagged_user.map((item) => {
+          if (item.username !== action.payload.username) return item;
+        
+          const newCoordinates = {
+            x: item.coordinates.x + action.payload.coordinates.x,
+            y: item.coordinates.y + action.payload.coordinates.y
+          };
+        
+          return {
+            ...item,
+            coordinates: newCoordinates
+          };
+        });
+      }
+    },
+    changeAddingTaggedUser: (state, action) => {
+      state.addingTaggedUser = action.payload
+    },
+    changeAddingTagCoordinates: (state, action) => {
+      state.addingTagCoordinates = action.payload
+    },
+    
+    
   },
 });
 
-export const { addPostMedia, changeActiveIndex, deleteActiveIndex, swapPostMedia, updateImageTransform, saveCroppedImage} = createPostSlice.actions;
+export const { addPostMedia, changeActiveIndex, deleteActiveIndex, swapPostMedia, updateImageTransform, saveCroppedImage, changeMediaAlt, addMediaUserTag, changeAddingTaggedUser, changeAddingTagCoordinates, removeMediaUserTag, moveMediaUserTagCoordinates} = createPostSlice.actions;
 export default createPostSlice.reducer;
