f5d41978220ba9da8e59e392efb2cc03bbf2f260
unknown <omp13821380@gmail.com>
2025-04-21 01:27:40 +0330

    changing folders to ignore user layout inside saved folders

---
+++ src/app/[id]/(no-layout)/saved/[folderName]/layout.tsx
@@ -0,0 +1,72 @@
+'use client'
+
+import SideBar from "@/components/SideBar"
+import { UnfollowPopup, UserList } from "@/components/SinglePost"
+import { useClickOutside } from "@/hooks/useClickOutside"
+import { changeCommentId, changeListTitle, changeListUrl, clearUserList, remove } from "@/store/slices/postSlice"
+import { changeUnfollow } from "@/store/slices/userSlice"
+import { RootState } from "@/store/store"
+import React, { useEffect, useRef, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+
+export default function PostLayout({children}:{children:React.ReactNode}){
+    const dispatch = useDispatch()
+    const unfollowDetail = useSelector((state: RootState) => state.currentUser.unfollowDetail);
+    const commentId = useSelector((state: RootState) => state.popupPost.commentId);
+    const listTitle = useSelector((state: RootState) => state.popupPost.listTitle);
+    const postDetail = useSelector((state: RootState) => state.popupPost.postDetail);
+    const userListRef = useRef(null)
+    const userHoverPreviewRef = useRef(null)
+    const unfollowPopupRef = useRef<HTMLElement | null>(null)
+    const [userListType,setUserListType] = useState<'likeList' | 'followerList' | 'followingList' | 'commentlikeList' | null>(null)
+    const [userListTarget,setUserListTarget] = useState<string | null>(null)
+    const [isUnfollowInList,setIsUnfollowInList] = useState(false)
+    useClickOutside([userListRef, userHoverPreviewRef], () => !unfollowDetail ? dispatch(changeListTitle(null)) : {});
+    useClickOutside(unfollowPopupRef, () => dispatch(changeUnfollow(null)));
+    function checkUserListTypeAndTarget(){
+        if(commentId){
+            setUserListType('commentlikeList')
+            setUserListTarget(commentId)
+        }
+        else if(listTitle == 'Likes'){
+            setUserListType('likeList')
+            setUserListTarget(postDetail.id)
+        }
+    }
+    useEffect(()=>{
+        if(listTitle){
+            setIsUnfollowInList(true)
+            checkUserListTypeAndTarget()
+        }
+        else{
+            setUserListType(null)
+            setUserListTarget(null)
+            setIsUnfollowInList(false)
+            dispatch(clearUserList())
+            dispatch(changeListUrl(null))
+            dispatch(changeCommentId(null))
+        }                
+        
+    },[listTitle])
+    useEffect(()=>{
+        dispatch(remove())
+    },[])
+    return(
+        <>
+            <div className="flex">
+                <SideBar/>
+                <div className="flex justify-center w-full md:w-[100vw-72px] xl:w-10/12">
+                    <div className="w-full md:px-4 xl:px-0 lg:w-[930px]">
+                        {children}
+                    </div>
+                </div>
+            </div>
+            {userListType && userListTarget &&
+                <UserList listType={userListType} targetId={userListTarget} ref={userListRef} hoverPreviewRef={userHoverPreviewRef} closePopup={()=>dispatch(changeListTitle(null))} />
+            }
+            {unfollowDetail &&
+                <UnfollowPopup inList={isUnfollowInList} ref={unfollowPopupRef}/>
+            }
+        </>
+    )
+}
---
+++ src/app/[id]/(no-layout)/saved/[folderName]/page.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import { fetchSimpleGet } from "@/api/simpleGet"
+import { IconLoading } from "@/components/Icons"
+import PostList from "@/components/PostList"
+import { PostPopupSlider } from "@/components/PostPopupSlider"
+import { addPostList, changePostListUrl, clearPostList } from "@/store/slices/postSlice"
+import { RootState } from "@/store/store"
+import { useParams } from "next/navigation"
+import { useEffect, useRef, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+
+export default function HashtagPage(){
+    const postList = useSelector((state: RootState)=> state.popupPost.postList)
+    const postUrl =  useSelector((state: RootState)=> state.popupPost.url)
+    const params = useParams()
+    const hashtag = decodeURIComponent(params.hashtag)
+    const [isLoading,setIsLoading] = useState(false)
+    const nextUrl = useRef('http://localhost:8000/hashtag/'+ hashtag)
+    const postListUrl = useSelector((state: RootState)=> state.popupPost.postListUrl)
+    const hasFetchedPostFirstTime = useRef(false);
+    const dispatch = useDispatch()
+    async function fetchPosts(){
+        const response = await fetchSimpleGet(hasFetchedPostFirstTime.current ? postListUrl : nextUrl.current)
+        const jsonRes = await response.json()
+        dispatch(changePostListUrl(jsonRes.next))
+        dispatch(addPostList(jsonRes.results))
+        nextUrl.current = jsonRes.next
+        setIsLoading(false)
+    }
+    useEffect(()=>{
+        dispatch(clearPostList())
+        dispatch(changePostListUrl('http://localhost:8000/hashtag/'+ hashtag))
+        setIsLoading(true)
+        window.addEventListener("scroll", handleScroll);
+    },[])
+    useEffect(()=>{
+        if(!isLoading) return
+        fetchPosts()
+        hasFetchedPostFirstTime.current = true
+    },[isLoading])
+
+    const handleScroll = () => {
+        if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 50 && !isLoading) {
+            if(!nextUrl.current) return
+            setIsLoading(true)
+        }
+      };
+    return(
+        <>
+            <div dir="ltr" className="text-xl pt-12 pb-3 font-semibold">#{hashtag}</div>
+            <PostList isHoverPreview={true} postList={postList} isReel={false} />
+            {postUrl && 
+                <PostPopupSlider/>
+            }
+            {isLoading && 
+                <div className="w-full flex justify-center p-4">
+                    <IconLoading className="size-8 fill-[#555555]" />
+                </div>
+            }
+        </>
+    )
+}
